1cda4af90964bfb8dccdfd35d1812a92e0c8d3ec
liziwl <leezisy@gmail.com>
2020-12-21 10:54:57 +0800

    update photo path

--- scripts/phototool.py
+++ scripts/phototool.py
@@ -1,28 +1,32 @@
 from PIL import Image
-import os
+from os.path import abspath, dirname, join
+from os import listdir
 import json
 
-img_root = "source/photos/images/data"
-json_output_path = "source/photos/photoslist.json"
+project_root = abspath(join(dirname(abspath(__file__)), "./.."))
 
-dirs = os.listdir(img_root)
+img_root = join(project_root, "source/photos/images/data")
+json_output_path = join(project_root, "source/photos/photoslist.json")
+
+dirs = listdir(img_root)
 _json_out = dict()
+count = 0
 
 for d in dirs:
-    path = os.path.join(img_root, d)
-    year, month = d.split("-")
-    _json_out[year] = dict()
-    _json_out[year][month] = dict()
-    imgs = os.listdir(path)
+    path = join(img_root, d)
+    _json_out[d] = dict()
+    imgs = listdir(path)
     for img in imgs:
         if not "thumbnail" in img:
-            im_path = os.path.join(path, img)
+            im_path = join(path, img)
             with Image.open(im_path) as im:
                 width, height = im.size
-                _json_out[year][month][img] = {
+                _json_out[d][img] = {
                     "w": width,
                     "h": height
                 }
+                count += 1
+_json_out['count'] = count
 
 with open(json_output_path, "w", encoding="utf-8") as fp:
     json.dump(_json_out, fp)
